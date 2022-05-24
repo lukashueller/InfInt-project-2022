21a390b59ab3bcd24294c003bd13c76f7e717bcb
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2022-05-24 21:06:12 +0200

    fix Kafka-Connect

--- connect/delete-config.sh
+++ connect/delete-config.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+### RB-ANNOUNCEMENTS
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink-rb-announcements.json"}
+KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
+
+CONNECTOR_NAME=$(jq -r .name $BASE_CONFIG)
+curl -Is -X DELETE $KAFKA_CONNECT_API/$CONNECTOR_NAME
+
+### WD-COMPANIES
+
+KAFKA_CONNECT_ADDRESS=${1:-localhost}
+KAFKA_CONNECT_PORT=${2:-8083}
+BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink-wd-companies.json"}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
 CONNECTOR_NAME=$(jq -r .name $BASE_CONFIG)
--- connect/push-config.sh
+++ connect/push-config.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+### RB-ANNOUNCEMENTS
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink-rb-announcements.json"}
+KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
+
+data=$(cat $BASE_CONFIG | jq -s '.[0]')
+curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
+
+### WD-COMPANIES
+
+KAFKA_CONNECT_ADDRESS=${1:-localhost}
+KAFKA_CONNECT_PORT=${2:-8083}
+BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink-wd-companies.json"}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
 data=$(cat $BASE_CONFIG | jq -s '.[0]')
